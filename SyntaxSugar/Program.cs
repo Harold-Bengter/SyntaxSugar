5f19f8b8eada396aaae9482aeba0fa1a04066369
Harold Bengter <Haroldbe55@yahoo.com>
2022-07-08 10:13:25 -0400

    Finished exercise

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,19 @@
+﻿//*Exercise 1*\\
+//int answer = 4;
+//string response;
+//if (answer = 9)
+//{
+//    response = answer + " is less than nine";
+//}
+//else
+//{
+//    response = answer + “greater than or equal to nine”;
+//};
+
+//Syntax Suagr
+
+var answer = 4;
+
+var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equals to nine";
+
+Console.WriteLine(response);
